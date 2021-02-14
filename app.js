38be12b8de0a68f2352c132df6b0c4b90f21fd45
Srabonifaria <76752669+Srabonifaria@users.noreply.github.com>
2021-02-14 15:37:38 +0600

    finish

--- app.js
+++ app.js
@@ -138,3 +138,14 @@ const toggleSpinner =() => {
   const spinner =document.getElementById('loading-spinner')
   spinner.classList.toggle('d-none');
 }
+$(document).ready(function(){
+  $('.next').click(function(){
+    $('.pagination').find('page-item.active').next().addClass('active');
+    $('.pagination').find('page-item.active').prev().removeClass('active');
+  })
+    $('.prev').click(function(){
+      $('.pagination').find('page-item.active').prev().addClass('active');
+      $('.pagination').find('page-item.active').next().removeClass('active');
+
+  })
+})
